71ccf7bc6efb3b702b6b1c652b07934d7416f3be
PhacPha <kittiphat240449@gmail.com>
2025-06-19 11:13:37 +0700

    1

--- download_phitakhon.py
+++ download_phitakhon.py
@@ -3,4 +3,4 @@ from icrawler.builtin import BingImageCrawler
 save_dir = '/Users/kittiphat/Downloads/รูปผีตาโขน'
 
 bing_crawler = BingImageCrawler(storage={'root_dir': save_dir}, downloader_threads=1)
-bing_crawler.crawl(keyword='ผีตาโขน', max_num=300, min_size=(300, 300))
+bing_crawler.crawl(keyword='ผีตาโขน', max_num=300, min_size=(640, 640))
