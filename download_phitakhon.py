211f7bec443e1aac4a3162d936b8ecd71cc86cf2
PhacPha <kittiphat240449@gmail.com>
2025-06-19 11:12:49 +0700

    ล่าสุด

--- download_phitakhon.py
+++ download_phitakhon.py
@@ -1,11 +1,6 @@
-from icrawler.builtin import GoogleImageCrawler
-import time
+from icrawler.builtin import BingImageCrawler
 
 save_dir = '/Users/kittiphat/Downloads/รูปผีตาโขน'
 
-google_crawler = GoogleImageCrawler(storage={'root_dir': save_dir}, downloader_threads=1)
-
-for idx, _ in enumerate(
-    google_crawler.crawl(keyword='ผีตาโขน', max_num=300, min_size=(300, 300), file_idx_offset=0)
-):
-    time.sleep(1)  # หน่วง 1 วินาที ต่อการโหลดแต่ละรูป (ช่วยลดโอกาสโดนแบน)
+bing_crawler = BingImageCrawler(storage={'root_dir': save_dir}, downloader_threads=1)
+bing_crawler.crawl(keyword='ผีตาโขน', max_num=300, min_size=(300, 300))
